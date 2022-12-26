03cd4f48b2e6f27d0e176ba4ef41683d5c4d3741
AlonaCh <aliona.chubenko@gmail.com>
2022-12-26 14:28:21 +0200

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by Alona Chubenko{" "}
-        <a
-          href="https://github.com/AlonaCh/react-weather-app.git"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          is open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by Alona Chubenko{" "}
+          <a
+            href="https://github.com/AlonaCh/react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            is open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
