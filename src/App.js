90f852e14dcbf85da4ffe9ea3c819b37afdc8834
AlonaCh <aliona.chubenko@gmail.com>
2022-12-26 10:36:24 +0200

    Added a link to my project

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Alona Chubenko
+        <a
+          href="https://github.com/AlonaCh/react-weather-app.git"
+          target="_blank"
+          rel="noreferrer"
+        >
+          is open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
